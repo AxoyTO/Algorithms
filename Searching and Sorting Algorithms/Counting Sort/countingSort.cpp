77413f07a7ccd00e82238598cfcae557d2647d2b
Tevfik Oguzhan AKSOY <toaxoy@protonmail.com>
2022-02-23 19:32:59 +0300

    (+) Counting Sort in C++

--- Searching and Sorting Algorithms/Counting Sort/countingSort.cpp
+++ Searching and Sorting Algorithms/Counting Sort/countingSort.cpp
@@ -1,28 +1,45 @@
 #include <algorithm>
-#include <array>
 #include <iostream>
+#include <typeinfo>
+#include <vector>
 
-template <typename TYPE, size_t SIZE>
-void print_array(std::array<TYPE, SIZE> array) {
+using Type = int;
+
+void printArray(std::vector<Type> array) {
   for (const auto& i : array) {
     std::cout << i << " ";
   }
   std::cout << "\n";
 }
 
-template <typename TYPE, size_t SIZE>
-void counting_sort(std::array<TYPE, SIZE>& array) {
-  TYPE* max = std::max_element(array.begin(), array.end());
-  std::array<TYPE, SIZE> count_array = {0};
-  print_array(count_array);
-  for (size_t j = 0; j < SIZE; j++){
-      
+void countingSort(std::vector<Type>& array) {
+  auto max_iter = std::max_element(array.begin(), array.end());
+  const int max = array[std::distance(array.begin(), max_iter)];
+  std::cout << "Maximum element is " << max << "\n";
+  std::vector<Type> output(array.size(), 0);
+  std::vector<Type> count(max + 1, 0);
+  for (size_t i = 0; i < array.size(); i++)
+    count[array[i]] += 1;
+
+  for (size_t i = 1; i < max + 1; i++) {
+    count[i] += count[i - 1];
+  }
+
+  int i = array.size() - 1;
+  while (i >= 0) {
+    output[count[array[i]] - 1] = array[i];
+    count[array[i]]--;
+    i -= 1;
+  }
+
+  for (i = 0; i < output.size(); i++) {
+    array[i] = output[i];
   }
 }
 
 int main() {
-  std::array<int, 7> foo_array = {6, 4, 9, 4, 3, 5, 1};
-  print_array(foo_array);
-  counting_sort(foo_array);
-  print_array(foo_array);
+  std::vector<Type> array = {6, 4, 9, 4, 3, 5, 1, 14, 7};
+  printArray(array);
+  countingSort(array);
+  printArray(array);
 }
